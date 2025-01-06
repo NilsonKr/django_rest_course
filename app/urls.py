9540a7de317f946e80c6bd55d9aa1358ca68ff79
nilsonkr <nuotrok444@gmail.com>
2025-01-06 03:04:46 -0500

    [DOCTORS] Implement action in viewset

--- app/urls.py
+++ app/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, Spec
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('patients/', include('patients.urls')),
-    path('', include('doctors.urls')),
+    path('api/', include('doctors.urls')),
     # DOCS
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
--- doctors/viewsSets.py
+++ doctors/viewsSets.py
@@ -1,7 +1,15 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Doctor
 from .serializers import DoctorSerializer
+
 
 class DoctorsViewSet(ModelViewSet):
   serializer_class = DoctorSerializer
   queryset = Doctor.objects.all()
+
+  @action(methods=['POST'], detail=True, url_path='send-inform-email')
+  def send_inform_email(self, request, pk):
+    instance = self.get_object()
+    return Response({ 'detail': f'Informe del Doctor {instance.first_name} {instance.last_name} has sido enviado al email {request.data["email"]}'})
